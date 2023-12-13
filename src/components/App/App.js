d0361a3860c4cf633fc525a28a3c12a21ded9134
Josiah Voight <jovoight@gmail.com>
2023-12-13 16:59:42 -0600

    more progress on data tracking

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import "./App.css";
 import LinkList from "../LinkList/LinkList.js";
 import Profile from "../Profile/Profile.js";
 import ShareBar from "../ShareBar/ShareBar.js";
+import DataTester from "../DataTester/DataTester.js";
 
 const App = () => {
 
@@ -32,82 +33,78 @@ const App = () => {
     }
   };
 
-  const pageVisit = class {
-    clicked = false;
-    converted = false;
-    timeOnSite = 0;
-    entryPoint = Document.referrer;
-    ipAddress = 
+  const [ip, setIp] = useState("");
+  const [location, setLocation] = useState({});
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [clicked, setClicked] = useState(false);
+  const [converted, setConverted] = useState(false);
+  const referrer = Document.referrer;
 
-    get click() {
-      return this.clicked;
-    };
-    get convert() {
-      return this.converted;
-    }
-    get time() {
-      return this.timeOnSite;
-    };
-    get entry() {
-      return this.entryPoint;
-    };
-
-    /**
-     * @param {boolean} click;
-     */
-    set click(click) {
-      this.clicked = click;
-    };
-    /**
-     * @param {boolean} convert
-     */
-    set convert(convert) {
-      this.converted = convert;
-    };
-    /**
-     * @param {number} time
-     */
-    set time(time) {
-      this.timeOnSite = time;
-    };
-    //No setter for entryPoint as this will never change
+  const postToDatabase = () => {
+    axios.post("api/endpoint", {
+      params: "params"
+    })
   };
 
-  let thisVisit;
-  let startTime;
-  let endTime;
+  const apiKey = "at_NRI9Zaar6ucd4U4mNbI3WwvAaVrJc";
 
-  
-  const handlePageShow = () => {
-    thisVisit = new pageVisit();
-    startTime = Date.now();
-  };
-  const handlePageHide = () => {
-    setTimeout(() => {
-      endTime = Date.now();
-      thisVisit.timeOnSite = endTime - startTime;
-    }, "1000")
-  };
+  useEffect(() => {
+    //On page show
+    const getIp = async () => {
+      const result = await axios.get("https://api.ipify.org");
+      setIp(result.data);
+    };
+    getIp();
+
+    const getLocation = async () => {
+      const result = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`);
+      setLocation(result.data.location);
+    };
+    getLocation();
+
+    setStartTime(Date.now());
+    //On page hide
+    return () => {
+      setEndTime(Date.now());
+      postToDatabase();
+    };
+  }, [ip]);
+
+
+
   const handleClick = () => {
-    thisVisit.clicked = true;
+    setClicked(true);
   };
-
-  window.addEventListener("pageshow", handlePageShow);
-  window.addEventListener("pagehide", handlePageHide);
+  const handleConverted = () => {
+    setConverted(true);
+  };
 
   return (
     <div className="App">
-      <Profile 
-        name={user.name}
-        profilePicPath={user.profilePicPath} 
-      />
-      <LinkList 
-        links={user.links}
-        linkClickHandler={handleClick}
-        subscribeLink={user.subscribeLink}
-      />
-      <ShareBar/>
+      <DataTester
+        ip={ip}
+        location={location}
+        startTime={startTime}
+        endTime={endTime}
+        clicked={clicked}
+        converted={converted}
+        referrer={referrer}
+      >
+        <Profile 
+          name={user.name}
+          profilePicPath={user.profilePicPath}
+        />
+        <LinkList 
+          links={user.links}
+          subscribeLink={user.subscribeLink}
+          linkClickHandler={handleClick}
+          linkConvertHandler={handleConverted}
+        />
+        <ShareBar/>
+      </DataTester>
     </div>
+    
   );
 };
 
---
+++ src/components/DataTester/DataTester.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./DataTester.css";
+
+const DataTester = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default DataTester;
