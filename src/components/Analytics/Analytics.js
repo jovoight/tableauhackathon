068fc7749290ff6c3549586c4088f0c36028b296
Joseph Armstrong <Joeagorn@gmail.com>
2024-02-12 06:19:48 -0800

    added placeholder dashboard to analytics page

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { TableauViz, TableauEventType, FilterUpdateType } from 'https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js';
 
 const Analytics = () => {
+    const [vizAttributes, setVizAttributes] = useState({});
+
+    let viz, workbook, activeSheet;
+
+    useEffect(() => {
+        setVizAttributes({
+            id: "analyticsViz",
+            className: "viz",
+            src: "https://public.tableau.com/views/WorldIndicators/GDPpercapita" // this is a placeholder
+        });
+
+        console.log("end of use effect");
+    })
+
     return (
         <div>
-            <p>Analytics</p>
+            <tableau-viz {...vizAttributes}></tableau-viz>
         </div>
     );
 };
